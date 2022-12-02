d1fcf38b0c0dd6cf66aa54eb5a9fc37b1582fa1f
Adriana Maftei <115250887+adriana-maftei@users.noreply.github.com>
2022-12-02 15:30:53 +0200

    Upload

---
+++ CodeGenerator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_barcode = new System.Windows.Forms.Button();
+            this.btn_qrcode = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(137, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 229);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Barcode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "QR Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(134, 346);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(389, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btn_barcode
+            // 
+            this.btn_barcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_barcode.Location = new System.Drawing.Point(529, 293);
+            this.btn_barcode.Name = "btn_barcode";
+            this.btn_barcode.Size = new System.Drawing.Size(141, 32);
+            this.btn_barcode.TabIndex = 5;
+            this.btn_barcode.Text = "Generate Bar";
+            this.btn_barcode.UseVisualStyleBackColor = true;
+            this.btn_barcode.Click += new System.EventHandler(this.btn_barcode_Click);
+            // 
+            // btn_qrcode
+            // 
+            this.btn_qrcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_qrcode.Location = new System.Drawing.Point(529, 344);
+            this.btn_qrcode.Name = "btn_qrcode";
+            this.btn_qrcode.Size = new System.Drawing.Size(141, 32);
+            this.btn_qrcode.TabIndex = 6;
+            this.btn_qrcode.Text = "Generate QR ";
+            this.btn_qrcode.UseVisualStyleBackColor = true;
+            this.btn_qrcode.Click += new System.EventHandler(this.btn_qrcode_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(529, 244);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(141, 32);
+            this.btn_save.TabIndex = 7;
+            this.btn_save.Text = "Save Code";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reset.Location = new System.Drawing.Point(529, 196);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(141, 32);
+            this.btn_reset.TabIndex = 8;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 396);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_qrcode);
+            this.Controls.Add(this.btn_barcode);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_barcode;
+        private System.Windows.Forms.Button btn_qrcode;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ CodeGenerator/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean generated = false;
+        Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+        Zen.Barcode.CodeQrBarcodeDraw qr = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+
+        private void btn_barcode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = barcode.Draw(textBox1.Text, 50);
+                generated = true;
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("First write code");
+                generated = false;
+            }
+        }
+
+        private void btn_qrcode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = qr.Draw(textBox2.Text, 50);
+                generated = true;
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("First write code");
+                generated = false; 
+            }
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+                if (generated)
+                {
+                    var sfd = new SaveFileDialog();
+                    sfd.Filter = "Image(*.jpg)|*.jpg|(*.*|*.*";
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                        pictureBox1.Image.Save(sfd.FileName);
+                }
+                else
+                    MessageBox.Show("First generate code");
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e) => ResetFields();
+
+        private void ResetFields()
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            pictureBox1.Image = null;
+            generated = false;
+        }
+    }
+}
